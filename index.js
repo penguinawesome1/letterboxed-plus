65267cb160adcfbc8f21306b7ac9933c3c164196
penguinawesome1 <owen.h.colley@gmail.com>
2024-11-06 12:22:15 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -18,20 +18,16 @@ prevBtn.addEventListener('click', () => {
 const currentLevel = localStorage.getItem("current_level");
 if (!currentLevel) localStorage.setItem("current_level", 1);
 for (let i = 1; i <= currentLevel; i++) {
-    const worldButton = document.getElementById(`${i}`);
+    const worldButton = document.getElementById(`world${i}`);
     worldButton.classList.remove("off");
     const score = localStorage.getItem(`score_level_${i}`);
-    console.log(score);
     if (!score) continue;
-    
-    const star1 = document.getElementById(`star${i*3}`);
-    star1.classList.remove("hidden");
-    if (score > 1) {
-        const star2 = document.getElementById(`star${i*3 - 1}`);
-        star2.classList.remove("hidden");
-    }
-    if (score == 3) {
-        const star3 = document.getElementById(`star${i*3 - 2}`);
-        star3.classList.remove("hidden");
+
+    for (let a = 0; a < score; a++) {
+        console.log("HI");
+        const star = document.createElement("div");
+        star.classList.add("star");
+        worldButton.querySelector(".star-wrapper").appendChild(star);
+        console.log(worldButton.querySelector(".star-wrapper").innerHTML);
     }
 }
